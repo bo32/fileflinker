5ba4f01e75c25a48198f113453020c7fe1829f0b
bo32 <david.bonnal@gmail.com>
2018-11-18 19:52:06 +0100

    solve warning 'ResourceWarning: unclosed file'

--- pdf_splitter.py
+++ pdf_splitter.py
@@ -19,43 +19,44 @@ class PDFSplitter:
         self.logger = logging.getLogger(PDFSplitter.__name__)
 
     def to_images(self, format, filepath, destination = config.DEFAULT_TEMP_FOLDER):
-        pdf = open(filepath, 'rb').read()
-        i = 0
-        startmark = b'\xff\xd8'
-        startfix = 0
-        endmark = b'\xff\xd9'
-        endfix = 2
-        njpg = 0
+        with open(filepath, 'rb') as input_pdf:
+            pdf = input_pdf.read()
+            i = 0
+            startmark = b'\xff\xd8'
+            startfix = 0
+            endmark = b'\xff\xd9'
+            endfix = 2
+            njpg = 0
 
-        filename = file_helper.get_filename_without_extension(filepath)
-        while True:
-            istream = pdf.find(b'stream', i)
-            if istream < 0:
-                if njpg == 0:
-                    raise Exception("The PDF couldn't be split.")
-                self.logger.info('Successfully split the PDF %s' % filepath)
-                self.logger.info('%d resulting files stored in %s' % (njpg, destination))
-                break
-            istart = pdf.find(startmark, istream, istream+20)
-            if istart < 0:
-                i = istream+20
-                continue
-            iend = pdf.find(b'endstream', istart)
-            if iend < 0:
-                raise Exception("Didn't find end of stream!")
-            iend = pdf.find(endmark, iend-20)
-            if iend < 0:
-                raise Exception("Didn't find end of JPG!")
-            
-            istart += startfix
-            iend += endfix
-            jpg = pdf[istart:iend]
+            filename = file_helper.get_filename_without_extension(filepath)
+            while True:
+                istream = pdf.find(b'stream', i)
+                if istream < 0:
+                    if njpg == 0:
+                        raise Exception("The PDF couldn't be split.")
+                    self.logger.info('Successfully split the PDF %s' % filepath)
+                    self.logger.info('%d resulting files stored in %s' % (njpg, destination))
+                    break
+                istart = pdf.find(startmark, istream, istream+20)
+                if istart < 0:
+                    i = istream+20
+                    continue
+                iend = pdf.find(b'endstream', istart)
+                if iend < 0:
+                    raise Exception("Didn't find end of stream!")
+                iend = pdf.find(endmark, iend-20)
+                if iend < 0:
+                    raise Exception("Didn't find end of JPG!")
+                
+                istart += startfix
+                iend += endfix
+                jpg = pdf[istart:iend]
 
-            if not file_helper.does_directory_exist(destination):
-                file_helper.create_directory(destination)
-            jpgfile = open(os.path.join(destination, '%s%d.%s' % (filename, njpg, format)), 'wb')
-            jpgfile.write(jpg)
-            jpgfile.close()
-            
-            njpg += 1
-            i = iend
+                if not file_helper.does_directory_exist(destination):
+                    file_helper.create_directory(destination)
+                jpgfile = open(os.path.join(destination, '%s%d.%s' % (filename, njpg, format)), 'wb')
+                jpgfile.write(jpg)
+                jpgfile.close()
+                
+                njpg += 1
+                i = iend
